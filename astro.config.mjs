31c3a1b44715314f4117c30ed95e02081e5e5b0e
Florian Pichler <pichfl@ylk.gd>
2026-03-15 00:20:35 +0100

    Preserve symlinks

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import { defineConfig } from 'astro/config';
 
 import { rehypeRewriteImages } from './src/lib/rehype-rewrite-images.js';
 
-
 // https://astro.build/config
 export default defineConfig({
 	devToolbar: {
@@ -15,7 +14,12 @@ export default defineConfig({
 	output: 'static',
 	site: 'https://blg.ylk.gd',
 	integrations: [mdx(), sitemap()],
-  markdown: {
+	markdown: {
 		rehypePlugins: [rehypeRewriteImages],
+	},
+	vite: {
+		resolve: {
+			preserveSymlinks: true,
+		},
 	},
 });
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,27 +4,32 @@ import { PAGE_SIZE, SITE_DESCRIPTION, SITE_TITLE } from '../consts';
 import { postSchema } from '../content.config';
 
 export async function GET(context) {
-  const importedPosts = import.meta.glob('../content/posts/**/*.md', { eager: true });
-  const posts = Object.values(importedPosts).map(post => ({
-    ...post,
-    data: postSchema.parse(post.frontmatter)
-  })).sort((a, b) => b.data.date - a.data.date).slice(0, PAGE_SIZE);
+	const importedPosts = import.meta.glob('../content/posts/**/*.md', { eager: true });
+	const posts = Object.values(importedPosts)
+		.map((post) => ({
+			...post,
+			data: postSchema.parse(post.frontmatter),
+		}))
+		.sort((a, b) => b.data.date - a.data.date)
+		.slice(0, PAGE_SIZE);
 
-  return rss({
-    // TODO: Style feed?
-    // stylesheet: '/rss/pretty.xsl',
+	return rss({
+		// TODO: Style feed?
+		// stylesheet: '/rss/pretty.xsl',
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-    items: await Promise.all(posts.map(async (post) => {
-      const { title, date: pubDate, slug } = post.data;
+		items: await Promise.all(
+			posts.map(async (post) => {
+				const { title, date: pubDate, slug } = post.data;
 
-      return {
-        title,
-        pubDate,
-        link: `/${slug}/`,
-        content: await post.compiledContent(),
-      };
-    })),
+				return {
+					title,
+					pubDate,
+					link: `/${slug}/`,
+					content: await post.compiledContent(),
+				};
+			})
+		),
 	});
 }
